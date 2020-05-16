9e78a3340953f485e1721b8400830c1e2889b807
Simon Templer <simon@wetransform.to>
2020-05-16 04:05:37 +0200

    limit user agent family matches to a smaller range of agents

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ var assert = require('assert'),
     path = require('path'),
     fs = require('fs'),
     yaml = require('yamlparser'),
-	refImpl = require('uap-ref-impl')(readYAML('../regexes.yaml'));
+	refImpl = require('uap-ref-impl')(filterRegexes(readYAML('../regexes.yaml')));
 
 function readYAML(fileName) {
     var file = path.join(__dirname, fileName);
@@ -10,23 +10,48 @@ function readYAML(fileName) {
     return yaml.eval(data);
 }
 
+function filterRegexes(yaml) {
+  var result = {};
+  for (var category in yaml) {
+    var regexList = yaml[category];
+
+    result[category] = regexList.filter(regex => !regex.skip).map(regex => {
+      if (regex.ignore) {
+        regex.family_replacement = 'Other';
+      }
+      return regex;
+    });
+  }
+
+  return result;
+}
+
 function msg(name, actual, expected) {
   return "Expected " + name + " to be " + JSON.stringify(expected) + " got " + JSON.stringify(actual) + " instead.";
 }
 
 ['../test_resources/firefox_user_agent_strings.yaml', '../tests/test_ua.yaml', '../test_resources/pgts_browser_list.yaml',
-  '../test_resources/opera_mini_user_agent_strings.yaml','../test_resources/podcasting_user_agent_strings.yaml'].forEach(function(fileName) {
+  '../test_resources/opera_mini_user_agent_strings.yaml',/*'../test_resources/podcasting_user_agent_strings.yaml'*/].forEach(function(fileName) {
   var fixtures = readYAML(fileName).test_cases;
+  var skipFamilies = readYAML('../tests/skip_ua.yaml').families;
   suite(fileName, function() {
     fixtures.forEach(function(f) {
-      test(f.user_agent_string, function() {
-        var ua = refImpl.parse(f.user_agent_string).ua;
-        fixFixture(f, ['major', 'minor', 'patch']);
-        assert.strictEqual(ua.family, f.family, msg('ua.family', ua.family, f.family));
-        assert.strictEqual(ua.major, f.major, msg('ua.major', ua.major, f.major));
-        assert.strictEqual(ua.minor, f.minor, msg('ua.minor', ua.minor, f.minor));
-        assert.strictEqual(ua.patch, f.patch, msg('ua.patch', ua.patch, f.patch));
-      });
+      if (!f.ignore) {
+        test(f.user_agent_string, function() {
+          var ua = refImpl.parse(f.user_agent_string).ua;
+          fixFixture(f, ['major', 'minor', 'patch']);
+          if (!f.skip && !skipFamilies.some(family => family == f.family )) {
+            assert.strictEqual(ua.family, f.family, msg('ua.family', ua.family, f.family));
+            assert.strictEqual(ua.major, f.major, msg('ua.major', ua.major, f.major));
+            assert.strictEqual(ua.minor, f.minor, msg('ua.minor', ua.minor, f.minor));
+            assert.strictEqual(ua.patch, f.patch, msg('ua.patch', ua.patch, f.patch));
+          }
+          else {
+            // if the tests should be skipped, expect Other
+            assert.strictEqual(ua.family, 'Other', msg('ua.family', ua.family, 'Other'));
+          }
+        });
+      }
     });
   });
 });
